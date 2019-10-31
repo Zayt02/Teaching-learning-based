b168fd87062c77eac49fa97b002e7b67572066e2
Tan Hoang <36371667+Zayt02@users.noreply.github.com>
2019-10-31 15:39:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import os
 import time
-import xlsxwriter as writer
+import csv
 from os.path import join
 import copy
 
 from TLBO import TLBO
 from Network import Network
 
-<<<<<<< HEAD
-data1_dir = os.getcwd() + '\\DataICC\\Sen1'
-data2_dir = os.getcwd() + '\\ResultICC\\Sen1'
-=======
+# <<<<<<< HEAD
+# data1_dir = os.getcwd() + '\\DataICC\\Sen1'
+# data2_dir = os.getcwd() + '\\ResultICC\\Sen1'
+# =======
 data1_dir = os.getcwd() + '/DataICC/Sen1'
 data2_dir = os.getcwd() + '/ResultICC/Sen1'
->>>>>>> de629cf682ca0fd65605642c77f213af86bb6fbd
+# >>>>>>> de629cf682ca0fd65605642c77f213af86bb6fbd
 # print(data2_dir)
-workbook = writer.Workbook('Result.xlsx')
-worksheet = workbook.add_worksheet()
+if "result.csv" not in os.listdir(os.getcwd()):
+    with open("result.csv", mode="w") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['File Name', 'Best - Num Dead', 'Best - Std of Remaining Time',
+                         'Worst - Num Dead', 'Worst - Std of Remaining Time', 'Average num dead', 'Time Execute'])
 
-offset = 0
-rows = 0
 for sub_dir1 in os.listdir(data1_dir):
     des1 = [join(data1_dir, sub_dir1, f) for f in os.listdir(join(data1_dir, sub_dir1))]
     des2 = [join(data2_dir, sub_dir1, f) for f in os.listdir(join(data2_dir, sub_dir1))]
@@ -39,7 +40,7 @@ for sub_dir1 in os.listdir(data1_dir):
         best = copy.copy(best_std)
         worst = copy.copy(best_std)
         avg = best.fitness[0]
-        for loop in range(1, 20):
+        for loop in range(1, 30):
             alg = TLBO(net)
             best_std = alg.loop()
             avg += best_std.fitness[0]
@@ -50,21 +51,15 @@ for sub_dir1 in os.listdir(data1_dir):
             # print(TLBO.print_best(alg, best))
         print("Best: ", TLBO.print_best(alg, best))
         print("Worst: ", TLBO.print_best(alg, worst))
-        avg = round(avg/20, 2)
+        avg = round(avg/30, 2)
         time_len = round(time.time() - start, 2)
+        with open("result.csv", mode="a") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow([file_name[i], best.fitness[0], round(best.fitness[1], 2),
+                         worst.fitness[0], round(worst.fitness[1], 2), avg, time_len])
 
-        worksheet.write(row, 0, file_name[i])
-        worksheet.write(row, 1, best.fitness[0])
-        worksheet.write(row, 2, round(best.fitness[1], 2))
-        worksheet.write(row, 3, worst.fitness[0])
-        worksheet.write(row, 4, round(worst.fitness[1], 2))
-        worksheet.write(row, 5, avg)
-        worksheet.write(row, 6, time_len)
-        row += 1
         # break
     # break
-
-workbook.close()
 
 # net = Network(file_path)
 # alg = TLBO(net)
